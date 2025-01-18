c6fb974d969938dc604c7e6d499957217d04692b
Parker Macdonald <parker_macdonald@mines.edu>
2025-01-18 01:03:09 -0700

    i/o redirection with >

--- src/execute.c
+++ src/execute.c
@@ -1,18 +1,28 @@
+#include "execute.h"
+#include "builtins.h"
+#include "enviroment.h"
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include "builtins.h"
-#include "enviroment.h"
-#include "execute.h"
 
-int spawn_process(char **const argv) {
+int spawn_process(char **const argv, const char *const in_path,
+                  const char *const out_path, const char *const err_path) {
   pid_t pid = fork();
 
   // child
   if (pid == 0) {
-    char** envp = env_get_array();
+    char **envp = env_get_array();
+
+    if (out_path != NULL) {
+      if (freopen(out_path, "w", stdout) == NULL) {
+        perror(out_path);
+        exit(EXIT_FAILURE);
+      }
+    }
+
     int status = execvpe(argv[0], argv, envp);
     free(envp);
 
@@ -45,12 +55,29 @@ int execute(char **const argv) {
   if (argv[0] == NULL) {
     return EXIT_SUCCESS;
   }
-  
+
   for (size_t i = 0; i < NUM_OF_BUILTINS; i++) {
     if (strcmp(argv[0], builtins[i]) == 0) {
       return (*builtin_fns[i])(argv);
     }
   }
 
-  return spawn_process(argv);
+  char *in_fd = NULL;
+  char *out_fd = NULL;
+  char *err_fd = NULL;
+
+  for (size_t i = 0; argv[i] != NULL; i++) {
+    if (strcmp(argv[i], ">") == 0) {
+      if (argv[i + 1] == NULL) {
+        fprintf(stderr, "Expected filename after '>'\n");
+        return EXIT_FAILURE;
+      }
+
+      out_fd = argv[i + 1];
+
+      argv[i] = NULL;
+    }
+  }
+
+  return spawn_process(argv, in_fd, out_fd, err_fd);
 }
