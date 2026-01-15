0b95881b5fa20135eccc7266d3cae77cee8f4b03
Larry the Pig <larrythepig123@gmail.com>
2026-01-15 10:17:45 -0700

    remove useless const from function protypes

--- src/builtins/builtins.h
+++ src/builtins/builtins.h
@@ -1,17 +1,17 @@
 #ifndef BUILTINS_H
 #define BUILTINS_H
 
-int builtin_cd(char **const argv);
+int builtin_cd(char **argv);
 
-int builtin_help(char **const argv);
+int builtin_help(char **argv);
 
-int builtin_exit(char **const argv);
+int builtin_exit(char **argv);
 
-int builtin_export(char **const argv);
+int builtin_export(char **argv);
 
-int builtin_history(char **const argv);
+int builtin_history(char **argv);
 
-int builtin_true(char **const argv);
+int builtin_true(char **argv);
 
 int builtin_false(char **argv);
 
