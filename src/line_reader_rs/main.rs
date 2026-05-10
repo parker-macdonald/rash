3876827bb878e2ea7e90be82837aa753e68541e5
Larry the Pig <larrythepig123@gmail.com>
2026-05-10 01:47:25 -0600

    also possible to just use std

--- src/line_reader_rs/main.rs
+++ src/line_reader_rs/main.rs
@@ -1,16 +1,3 @@
-#![no_std]
-#![no_main]
-
-use core::panic::PanicInfo;
-
-#[panic_handler]
-unsafe fn panic(_: &PanicInfo) -> ! {
-  extern "C" {
-    pub fn abort() -> !;
-  }
-  abort();
-}
-
 #[no_mangle]
 pub fn add(a: u32, b: u32) -> u32 {
   a + b
