004b7ecd8d782ac6b2afdd6e5452fbc39945632d
Parker Macdonald <parker_macdonald@mines.edu>
2026-07-04 21:12:36 -0600

    revert true.c

--- src/builtins/commands/true.c
+++ src/builtins/commands/true.c
@@ -1,9 +1,7 @@
 #include "builtins/builtins.h"
-#include "builtins/utils.h"
-#include <stdio.h>
 
 int builtin_true(char **argv) {
-  printf("%d\n", count_argv(argv));
+  (void)argv;
 
   return 0;
 }
